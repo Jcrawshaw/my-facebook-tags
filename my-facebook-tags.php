1566000929da7bc3c28dd429373be5103717eda0
Jennifer Crawshaw <jennycrawshaw@gmail.com>
2016-05-18 16:22:31 -0400

    first commit

---
+++ my-facebook-tags.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: My Facebook Tags
+ * Plugin URI: http://jcrawshaw.github.io
+ * Description: This plugin adds some Facebook Open Graph tags to our single posts.
+ * Version: 1.0.0
+ * Author: Jenny Crawshaw
+ * Author URI: http://jcrawshaw.github.io
+ * License: GPL2
+ */
